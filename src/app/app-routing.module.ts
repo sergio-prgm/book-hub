960fb43d87f972e3383e39c4be6b17b3d37fc983
Sergio Dev <sergio-dev@outlook.com>
2022-01-25 11:32:52 +0100

    Add Router + preliminary routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { TusLibrosComponent } from './components/tus-libros/tus-libros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LandingComponent, /* pathMatch: 'full' */},
+  {path: 'sobre-nosotros', component: AboutUsComponent},
+  {path: 'mis-libros', component: TusLibrosComponent},
+  {path: '**', redirectTo: '/'} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http'
+
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ComponentsComponent } from './navbar/components/components.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { CardComponent } from './components/card/card.component';
+import { TusLibrosComponent } from './components/tus-libros/tus-libros.component';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+
+import { ApiRestService } from './services/api-rest.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ComponentsComponent,
-    CardComponent
+    CardComponent,
+    TusLibrosComponent,
+    SearchbarComponent,
+    LandingComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApiRestService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
